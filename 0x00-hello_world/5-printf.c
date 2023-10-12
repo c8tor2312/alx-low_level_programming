5c425e5a8f08d1511748bb8473201d41027bf3b0
C8tor2312 <ahemenjeho2312@gmail.com>
2023-10-12 02:13:34 -0700

    Add script: 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print stream of characters with printf. End with a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
