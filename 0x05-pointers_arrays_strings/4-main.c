038d763b7691b98735c5ed232f464436133ca5c6
c8tor2312 <ahemenjehjo2312@gmail.com>
2023-10-23 06:12:41 +0100

    Update script: handle loops overflow

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -8,8 +8,11 @@
 int main(void)
 {
     char *str;
+    char *str_02;
 
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
     print_rev(str);
+    str_02 = "The";
+    print_rev(str_02);
     return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,18 @@
 void print_rev(char *s)
 {
 	int i;
+	int j;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	while (i >= 0)
+	j = i - 1;
+	while (0 <= j)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[j]);
+		j--;
 	}
 	_putchar('\n');
 }
