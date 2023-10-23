026ec8c6997dc66f093b5256f4c7fad5db037c3d
c8tor2312 <ahemenjehjo2312@gmail.com>
2023-10-23 20:11:54 +0100

    Add script: copy string to another destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string to another destination in memory
+ * @dest: destination
+ * @src: source
+ * Return: destination to the new string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+		
