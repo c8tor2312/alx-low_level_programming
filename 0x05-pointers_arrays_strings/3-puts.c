643671fe83c0e141358ac850ef884aa0916e6889
c8tor2312 <ahemenjehjo2312@gmail.com>
2023-10-23 02:40:19 +0100

    Add script: print string to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - write string to standard output
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
