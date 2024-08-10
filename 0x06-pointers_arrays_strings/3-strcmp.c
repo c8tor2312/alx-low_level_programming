1d3af8fed02010483e5a4fcade2d7cb86c5f5780
c8t0r2312 <ahemenjeho2312@gmail.com>
2024-08-10 19:47:16 +0100

    Resolve 103-infinit_add.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1 = s1 + 1;
-		s2 = s2 + 1;
-	}
-	return (0);
+    while (*s1 != '\0' && *s2 != '\0')
+    {
+        if (*s1 != *s2)
+            return (*s1 - *s2);
+        s1 = s1 + 1;
+        s2 = s2 + 1;
+    }
+    return (0);
 }
